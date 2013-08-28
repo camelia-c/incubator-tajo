12bde1a2f2c1bcb42ba3979275cf4879a542592f
Camelia <camelie_1985@yahoo.com>
2013-08-29 01:42:13 +0300

    Modified FilterPushdown with outer join restrictions in the new version of TAJO

--- tajo-core/tajo-core-backend/src/main/java/org/apache/tajo/engine/planner/rewrite/FilterPushDownRule.java
+++ tajo-core/tajo-core-backend/src/main/java/org/apache/tajo/engine/planner/rewrite/FilterPushDownRule.java
@@ -30,6 +30,13 @@ import org.apache.tajo.engine.planner.PlanningException;
 import org.apache.tajo.engine.planner.logical.*;
 import org.apache.tajo.engine.query.exception.InvalidQueryException;
 
+import org.apache.tajo.engine.eval.EvalType; 
+import org.apache.tajo.engine.eval.FieldEval;
+import org.apache.tajo.catalog.Column;
+import com.google.common.collect.Sets;
+import java.util.Set;
+import java.util.Iterator;
+
 import java.util.List;
 import java.util.Stack;
 
@@ -86,6 +93,100 @@ public class FilterPushDownRule extends BasicLogicalPlanVisitor<List<EvalNode>> 
       throws PlanningException {
     LogicalNode left = joinNode.getRightChild();
     LogicalNode right = joinNode.getLeftChild();
+
+    // camelia --
+    //here we should stop selection pushdown on the null supplying side(s) of an outer join
+    //get the two operands of the join operation as well as the join type
+    JoinType jt = joinNode.getJoinType(); 
+    if ((joinNode.hasJoinQual()==true)&&((jt==JoinType.LEFT_OUTER)||(jt==JoinType.RIGHT_OUTER)||(jt==JoinType.FULL_OUTER))){
+       if((joinNode.getJoinQual().getLeftExpr().getType() == EvalType.FIELD)&&(joinNode.getJoinQual().getRightExpr().getType() == EvalType.FIELD)){
+          String leftexprname = ((FieldEval) joinNode.getJoinQual().getLeftExpr()).getTableId();
+          String rightexprname = ((FieldEval) joinNode.getJoinQual().getRightExpr()).getTableId();
+          List<String> nullSuppliers = Lists.newArrayList();
+          String [] outerlineage = PlannerUtil.getLineage(joinNode.getLeftChild());
+          String [] innerlineage = PlannerUtil.getLineage(joinNode.getRightChild());
+          Set<String> o = Sets.newHashSet(outerlineage);
+          Set<String> i = Sets.newHashSet(innerlineage);
+
+          //some verification
+          if(jt==JoinType.FULL_OUTER){
+             nullSuppliers.add(leftexprname);
+             nullSuppliers.add(rightexprname);
+             //verify that these null suppliers are indeed in the o and i sets
+             if((i.contains(nullSuppliers.get(0))==false)&&(o.contains(nullSuppliers.get(0))==false))
+                throw new InvalidQueryException("Incorrect Logical Query Plan with regard to outer join");
+             if((i.contains(nullSuppliers.get(1))==false)&&(o.contains(nullSuppliers.get(1))==false))
+                throw new InvalidQueryException("Incorrect Logical Query Plan with regard to outer join");          
+
+          }
+          else if (jt==JoinType.LEFT_OUTER){
+             nullSuppliers.add(((ScanNode)joinNode.getRightChild()).getTableId()); 
+             //verify that this null supplier is indeed in the right sub-tree 
+             if (i.contains(nullSuppliers.get(0))==false)
+                 throw new InvalidQueryException("Incorrect Logical Query Plan with regard to outer join");
+            
+          }
+          else if (jt==JoinType.RIGHT_OUTER){
+             if (((ScanNode)joinNode.getRightChild()).getTableId().equals(rightexprname)==true) {
+                 nullSuppliers.add(leftexprname);
+          }
+          else {
+             nullSuppliers.add(rightexprname);                              
+          }
+          //verify that this null supplier is indeed in the left sub-tree
+          if (o.contains(nullSuppliers.get(0))==false)
+             throw new InvalidQueryException("Incorrect Logical Query Plan with regard to outer join");
+         }
+         
+         //retain in this outer join node's JoinQual those selection predicates related to the outer join's null supplier(s)
+          
+         List<EvalNode> matched2 = Lists.newArrayList();
+         for (EvalNode eval : cnf) {
+            
+            Set<Column> columnRefs = EvalTreeUtil.findDistinctRefColumns(eval);
+            Set<String> tableIds = Sets.newHashSet();
+            // getting distinct table references
+            for (Column col : columnRefs) {
+              if (!tableIds.contains(col.getTableName())) {
+                tableIds.add(col.getTableName());
+              }
+            }
+            
+            //if the predicate involves any of the null suppliers
+            boolean shouldKeep=false;
+            Iterator<String> it2 = nullSuppliers.iterator();
+            while(it2.hasNext()){
+              if(tableIds.contains(it2.next()) == true) {
+                   shouldKeep = true; 
+              }
+            }
+
+            if(shouldKeep == true) {
+                matched2.add(eval);
+            }
+            
+          }
+
+          //merge the retained predicates and establish them in the current outer join node. Then remove them from the cnf
+          EvalNode qual2 = null;
+          if (matched2.size() > 1) {
+             // merged into one eval tree
+             qual2 = EvalTreeUtil.transformCNF2Singleton(
+                        matched2.toArray(new EvalNode [matched2.size()]));
+          } else if (matched2.size() == 1) {
+             // if the number of matched expr is one
+             qual2 = matched2.get(0);
+          }
+          if (qual2 != null){
+             EvalNode conjQual2 = EvalTreeUtil.transformCNF2Singleton(joinNode.getJoinQual(), qual2);
+             joinNode.setJoinQual(conjQual2);
+             cnf.removeAll(matched2);
+          }
+          
+         //for the remaining cnf, push it as usual
+       }
+    }
+    // -- camelia
 
     visitChild(plan, left, stack, cnf);
     visitChild(plan, right, stack, cnf);
